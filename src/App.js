8aaafc03f972a1c0a77c04ba3ed1090186d4033d
Jakobi Matagi <jkmatagi@hotmail.com>
2021-01-22 10:50:13 -0700

    structure is almost set working no how to fetch the data correctly and some styling is done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { BASE_URL, API_KEY } from './Url/baseUrl';
+import Characters from './components/Character'
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+ const [swData, setSWData] = useState({})
 
+ useEffect(() => {
+  axios
+  .get(`${BASE_URL}${API_KEY}`)
+  .then((res) =>{
+setSWData(res.data.results)
+console.log(res.data.results);
+  })
+.catch((err) => console.log(err));
+ }, []);
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
@@ -12,7 +27,9 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Characters name={swData.name}/>
     </div>
+    
   );
 }
 
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,43 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+
+const Character = (props) =>{
+const {name} = props;
+return (
+    <StyleDiv>
+        <div>
+            <h1>test    name</h1>
+            
+        </div>
+    </StyleDiv>
+    
+)
+
+}
+
+const StyleDiv = styled.div`
+background-color: white;
+width: 50%;
+display: flex;
+flex-direction: row;
+justify-content: flex-start;
+margin-left: 25%;
+height: 60vh;
+
+h1{
+    font-size: 3rem;
+    
+    margin: 5%;
+    padding: 5%;
+}
+
+`
+
+
+
+
+
+
+export default Character;
